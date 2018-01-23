afaee72f950590878040671d6f33bb0c8c81aec5
rkking <rkking@live.com>
2018-01-23 12:33:17 +0530

    Updated look and contents
    Added stuffs

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/* 
+ * Copyright (C) 2018 Rajan Kumar
+ *  rkking@live.com
+ */
+
+
+      /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("menu_i").setAttribute("onclick","closeNav()");
+}
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("menu_i").setAttribute("onclick","openNav()");
+}
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
+var fsl = document.getElementById("fb");
+var xx = "https://www.facebook.com/sharer.php?u="+window.location.href;
+fsl.setAttribute("href",xx);
+var tsl = document.getElementById("twitter");
+var yy = "https://twitter.com/home?status="+window.location.href;
+tsl.setAttribute("href",yy);
+var lsl = document.getElementById("linkedin");
+var zz = "https://www.linkedin.com/shareArticle?mini=true&url="+window.location.href+"&title=HackersFriend.com&summary=&source=";
+lsl.setAttribute("href",zz);
